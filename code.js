13cc89e7ee1f80172fb504696b941a613bc47c6f
Devmontype <bmondal2741477@gmail.com>
2021-11-11 01:05:48 +0530

    files added

---
+++ code.js
@@ -0,0 +1,8 @@
+function string_add(str=""){
+
+    if(str.length==0){
+        return 0;
+    }
+}
+
+module.exports=string_add
---
+++ index.js
@@ -0,0 +1,3 @@
+const string_add=require('./code');
+
+console.log(string_add())
