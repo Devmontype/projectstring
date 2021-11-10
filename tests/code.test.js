75c38ad3031f28496f58132821f6c4c5848316db
Devmontype <bmondal2741477@gmail.com>
2021-11-11 01:26:00 +0530

    test case 3

--- tests/code.test.js
+++ tests/code.test.js
@@ -7,3 +7,7 @@ test('adder_fn should return 0',()=>{
 test('adder_fn should return 1',()=>{
     expect(adder_fn("1")).toBe(1)
 })
+
+test('adder_fn should return 3',()=>{
+    expect(adder_fn("1@2")).toBe(1)
+})
