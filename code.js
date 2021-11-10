3570bebddf9e91ed815e41e71bbc5e215fcaa51f
Devmontype <bmondal2741477@gmail.com>
2021-11-11 01:24:45 +0530

    test case 2

--- code.js
+++ code.js
@@ -1,7 +1,14 @@
 function string_add(str=""){
 
+    let sum=0;
     if(str.length==0){
         return 0;
+    }else{
+        let cm_sep=str.split(",");
+        for(let i=0;i<cm_sep.length;i++){
+            sum+=Number(cm_sep[i]);
+        }
+        return sum;
     }
 }
 
--- tests/code.test.js
+++ tests/code.test.js
@@ -3,3 +3,7 @@ const adder_fn=require('../code');
 test('adder_fn should return 0',()=>{
     expect(adder_fn("")).toBe(0)
 })
+
+test('adder_fn should return 1',()=>{
+    expect(adder_fn("1")).toBe(1)
+})
